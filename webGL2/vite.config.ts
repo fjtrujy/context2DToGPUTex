1b122ca3bbcd8ba0db1891c1050b7fd5f43e4ec5
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2025-03-13 12:14:49 +0100

    Update deployment github pages

--- webGL2/vite.config.ts
+++ webGL2/vite.config.ts
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
+function getBase() {
+    // GitHub Pages
+    if (process.env.GITHUB_PAGES === 'true') {
+        return '/context2DToGPUTex/webgl2/';
+    }
+    
+    // Local development or preview
+    return './';
+}
+
 export default defineConfig({
+    base: getBase(),
     plugins: [glsl()],
+    server: {
+        https: undefined, // Set to true if you need HTTPS
+        host: true, // Expose to all network interfaces
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp'
+        }
+    }
 });
--- webGPU/vite.config.ts
+++ webGPU/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 function getBase() {
     // GitHub Pages
     if (process.env.GITHUB_PAGES === 'true') {
-        return '/context2DToGPUTex/';
+        return '/context2DToGPUTex/webgpu/';
     }
     
     // Local development or preview
@@ -14,7 +14,7 @@ export default defineConfig({
     base: getBase(),
     assetsInclude: ['**/*.wgsl'],
     server: {
-        https: false, // Set to true if you need HTTPS
+        https: undefined, // Set to true if you need HTTPS
         host: true, // Expose to all network interfaces
         headers: {
             'Cross-Origin-Opener-Policy': 'same-origin',
